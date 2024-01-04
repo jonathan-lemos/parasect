a31ae6f9f2bb87fb5412fc2f5a00d40e41d419a1
Jonathan Lemos <jonathanlemos@protonmail.com>
2024-01-03 23:35:54 -0500

    parasect tests, removed orientation

--- src/algorithms/parasect.rs
+++ src/algorithms/parasect.rs
@@ -2,58 +2,49 @@ use std::{io, thread};
 use std::collections::HashMap;
 use num_cpus;
 use ibig::{ibig, IBig};
-use crate::algorithms::parasect::ParasectOrientation::*;
 use crate::algorithms::parasect::ParasectError::*;
 use crate::algorithms::parasect::ParasectPayloadAnswer::*;
 use crate::algorithms::parasect::ParasectPayloadResult::*;
 
-#[derive(PartialEq, Eq, Ord, PartialOrd, Hash, Copy, Clone)]
+#[derive(PartialEq, Eq, Ord, PartialOrd, Hash, Copy, Clone, Debug)]
 pub enum ParasectPayloadAnswer {
     Good,
     Bad,
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Debug)]
 pub enum ParasectPayloadResult {
     Continue(ParasectPayloadAnswer),
     Stop(String),
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Debug)]
 pub enum ParasectError {
-    ThreadError(io::Error),
     PayloadError(String),
     InconsistencyError(String),
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Hash)]
-pub enum ParasectOrientation {
-    GoodFirst,
-    BadFirst,
-}
-
-#[derive(PartialEq, Eq, Hash, Clone)]
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
 pub struct ParasectSettings<TPayload>
-    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Clone + Send + Sync + 'static {
+    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Send + Sync {
     low: IBig,
     high: IBig,
     payload: TPayload,
     before_level_callback: Option<fn(&[IBig]) -> ()>,
     after_level_callback: Option<fn(&HashMap<IBig, Result<ParasectPayloadAnswer, ParasectError>>) -> ()>,
     max_parallelism: usize,
-    orientation: Option<ParasectOrientation>,
 }
 
 impl<TPayload> ParasectSettings<TPayload>
-    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Clone + Send + Sync {
-    pub fn new(low: IBig, high: IBig, payload: TPayload) -> Self {
+    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Send + Sync {
+    pub fn new<A: Into<IBig>, B: Into<IBig>>(low: A, high: B, payload: TPayload) -> Self {
         return ParasectSettings {
-            low,
-            high,
+            low: low.into(),
+            high: high.into(),
             payload,
             before_level_callback: None,
             after_level_callback: None,
             max_parallelism: num_cpus::get(),
-            orientation: None,
         };
     }
 
@@ -69,11 +60,6 @@ impl<TPayload> ParasectSettings<TPayload>
 
     pub fn with_max_parallelism(mut self, parallelism: usize) -> Self {
         self.max_parallelism = parallelism;
-        self
-    }
-
-    pub fn with_orientation(mut self, orientation: ParasectOrientation) -> Self {
-        self.orientation = Some(orientation);
         self
     }
 }
@@ -128,50 +114,47 @@ pub(crate) fn payload_result_to_result(payload_result: ParasectPayloadResult)
 
 pub(crate) fn parallel_execute_payload<TPayload>(indices: &[IBig], payload: &TPayload)
                                                  -> HashMap<IBig, Result<ParasectPayloadAnswer, ParasectError>>
-    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Send + Sync + Clone + 'static {
+    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Send + Sync {
+    thread::scope(|scope| {
+        let threads = indices.into_iter()
+            .map(|x| {
+                (x.clone(), scope.spawn(|| payload(x.clone())))
+            });
 
-    let threads = indices.into_iter()
-        .map(|x| {
-            let xclone = x.clone();
-            let payload_clone = payload.clone();
-            (x.clone(), thread::spawn(move || payload_clone(xclone)))
-        });
-
-    threads.map(|(n, t)| {
-        let t_result = t.join().expect("spawned parasect thread panicked");
-        (n, payload_result_to_result(t_result))
-    }).collect()
+        threads.map(|(n, t)| {
+            let t_result = t.join().expect("spawned parasect thread panicked");
+            (n, payload_result_to_result(t_result))
+        }).collect()
+    })
 }
 
 pub(crate) fn find_bounds_from_bool_list(
-    sorted_bool_results: &[(IBig, bool)], original_low: IBig, original_high: IBig, orientation: ParasectOrientation)
+    sorted_bool_results: &[(IBig, bool)], original_low: IBig, original_high: IBig)
     -> Result<(IBig, IBig), ParasectError> {
-    let transition_marker = match orientation {
-        GoodFirst => false,
-        BadFirst => true
-    };
+    let mut last_true: Option<IBig> = None;
+    let mut first_false: Option<IBig> = None;
 
-    let mut last_before: Option<IBig> = None;
-    let mut first_transition: Option<IBig> = None;
     for (n, b) in sorted_bool_results {
-        if *b == transition_marker {
-            first_transition = first_transition.or_else(|| Some(n.clone()));
+        if !*b {
+            first_false = first_false.or_else(|| Some(n.clone()));
         } else {
-            if let Some(idx) = first_transition {
-                let (sequence, outlier) = match orientation {
-                    GoodFirst => ("bad", "good"),
-                    BadFirst => ("good", "bad")
-                };
-                return Err(InconsistencyError(format!("Found a sequence of {} starting at {}, then another {} at {}.", sequence, idx, outlier, n)));
+            if let Some(idx) = first_false {
+                return Err(InconsistencyError(format!("Found a sequence of bad starting at {}, then another good at {}.", idx, n)));
             }
-            last_before = Some(n.clone());
+            last_true = Some(n.clone());
         }
     }
 
-    match (last_before, first_transition) {
-        (Some(b), Some(a)) => Ok((b, a - 1)),
-        (None, Some(a)) => Ok((original_low, a - 1)),
-        (Some(b), None) => Ok((b, original_high)),
+    match (last_true, first_false) {
+        (Some(b), Some(a)) => Ok((b + 1, a)),
+        (None, Some(a)) => Ok((original_low, a)),
+        (Some(b), None) => {
+            if &b + 1 > original_high {
+                Err(InconsistencyError(format!("All values in [{}, {}] are good.", original_low, original_high)))
+            } else {
+                Ok((b + 1, original_high))
+            }
+        },
         _ => panic!("Cannot find bounds of an empty result sequence.")
     }
 }
@@ -179,8 +162,7 @@ pub(crate) fn find_bounds_from_bool_list(
 pub(crate) fn find_new_bounds(
     level_map: HashMap<IBig, Result<ParasectPayloadAnswer, ParasectError>>,
     original_low: IBig,
-    original_high: IBig,
-    orientation: ParasectOrientation) -> Result<(IBig, IBig), ParasectError> {
+    original_high: IBig) -> Result<(IBig, IBig), ParasectError> {
     let mut return_error: Option<ParasectError> = None;
 
     let mut bool_results: Vec<(IBig, bool)> = Vec::new();
@@ -197,41 +179,15 @@ pub(crate) fn find_new_bounds(
         return Err(e);
     }
 
-    find_bounds_from_bool_list(&bool_results, original_low, original_high, orientation)
-}
+    bool_results.sort();
 
-pub(crate) fn determine_orientation<TPayload>(low: IBig, high: IBig, payload: &TPayload) -> Result<(IBig, IBig, ParasectOrientation), ParasectError>
-    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Clone + Send + Sync + 'static {
-    let mut bounds_result_map = parallel_execute_payload(&[low.clone(), high.clone()], payload);
-
-    let low_result = bounds_result_map.remove(&low).unwrap();
-    let high_result = bounds_result_map.remove(&high).unwrap();
-
-    match (low_result, high_result) {
-        (Ok(Good), Ok(Bad)) => Ok((low, high - 1, GoodFirst)),
-        (Ok(Bad), Ok(Good)) => Ok((low, high - 1, BadFirst)),
-        (Ok(Good), Ok(Good)) => Err(InconsistencyError(format!("Both the low point {} and high point {} are good.", low, high))),
-        (Ok(Bad), Ok(Bad)) => Err(InconsistencyError(format!("Both the low point {} and high point {} are bad.", low, high))),
-        (Err(e), _) => Err(e),
-        (_, Err(e)) => Err(e)
-    }
+    find_bounds_from_bool_list(&bool_results, original_low, original_high)
 }
 
 pub fn parasect<TPayload>(settings: ParasectSettings<TPayload>) -> Result<IBig, ParasectError>
-    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Clone + Send + Sync {
-
+    where TPayload: (Fn(IBig) -> ParasectPayloadResult) + Send + Sync {
     let mut low = settings.low;
     let mut high = settings.high;
-
-    let orientation = if let Some(o) = settings.orientation {
-        o
-    } else {
-        let (new_low, new_high, orientation) =
-            determine_orientation(low, high, &settings.payload)?;
-        low = new_low;
-        high = new_high;
-        orientation
-    };
 
     while low < high {
         let indices = compute_parasect_indices(&low, &high, settings.max_parallelism);
@@ -246,7 +202,9 @@ pub fn parasect<TPayload>(settings: ParasectSettings<TPayload>) -> Result<IBig, 
             after_cb(&level_results);
         }
 
-        (low, high) = find_new_bounds(level_results, low, high, orientation)?;
+        let (new_low, new_high) = find_new_bounds(level_results, low.clone(), high.clone())?;
+        assert_ne!((low.clone(), high.clone()), (new_low.clone(), new_high.clone()));
+        (low, high) = (new_low, new_high);
     }
 
     Ok(low)
@@ -256,12 +214,17 @@ pub fn parasect<TPayload>(settings: ParasectSettings<TPayload>) -> Result<IBig, 
 mod tests {
     use std::collections::HashSet;
     use std::iter::zip;
+    use std::sync::Mutex;
     use ibig::ops::Abs;
     use quickcheck::*;
     use super::*;
 
+    fn ibig(i: isize) -> IBig {
+        IBig::from(i)
+    }
+
     fn ibig_vec(v: &[isize]) -> Vec<IBig> {
-        v.into_iter().map(|x| IBig::from(*x)).collect()
+        v.into_iter().map(|x| ibig(*x)).collect()
     }
 
     #[test]
@@ -285,7 +248,7 @@ mod tests {
 
         let result = ibig_range(&lo_ibig, &hi_ibig);
 
-        assert_eq!(result.len(), (hi as isize - lo as isize) as usize + 1);
+        assert_eq!(result.len(), (hi as i64 - lo as i64) as usize + 1);
         assert_eq!(result.first().unwrap(), &lo_ibig);
         assert_eq!(result.last().unwrap(), &hi_ibig);
 
@@ -343,6 +306,205 @@ mod tests {
                 TestResult::passed()
             }
             _ => TestResult::failed()
+        }
+    }
+
+    #[test]
+    fn test_parallel_execute_payload() {
+        let indices = ibig_vec(&[-100, -69, 0, 1, 42]);
+        let statuses = Mutex::new(HashMap::from([
+            (ibig(-100), Continue(Good)),
+            (ibig(-69), Continue(Good)),
+            (ibig(0), Continue(Bad)),
+            (ibig(1), Continue(Bad)),
+            (ibig(42), Stop("amogus".into()))]));
+
+        let expected = HashMap::from([
+            (ibig(-100), Ok(Good)),
+            (ibig(-69), Ok(Good)),
+            (ibig(0), Ok(Bad)),
+            (ibig(1), Ok(Bad)),
+            (ibig(42), Err(PayloadError("amogus".into())))]);
+
+        let actual = parallel_execute_payload(&indices, &|idx| {
+            let mut ss = statuses.lock().unwrap();
+            ss.remove(&idx).unwrap()
+        });
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_find_bounds_from_bool_list_peak() {
+        let result = find_bounds_from_bool_list(&[
+            (ibig(0), true),
+            (ibig(2), true),
+            (ibig(4), false),
+            (ibig(6), false),
+            (ibig(8), false)
+        ], ibig(-2), ibig(10));
+
+        match result {
+            Ok((lo, hi)) => assert_eq!((lo, hi), (ibig(3), ibig(4))),
+            Err(e) => panic!("expected (3, 4), but got error {:?}", e)
+        }
+    }
+
+    #[test]
+    fn test_find_bounds_from_bool_list_all_bad() {
+        let result = find_bounds_from_bool_list(&[
+            (ibig(0), false),
+            (ibig(2), false),
+            (ibig(4), false),
+            (ibig(6), false),
+            (ibig(8), false)
+        ], ibig(-2), ibig(10));
+
+        match result {
+            Ok((lo, hi)) => assert_eq!((lo, hi), (ibig(-2), ibig(0))),
+            Err(e) => panic!("expected (-2, 0), but got error {:?}", e)
+        }
+    }
+
+    #[test]
+    fn test_find_bounds_from_bool_list_all_bad_boundary() {
+        let result = find_bounds_from_bool_list(&[
+            (ibig(0), false),
+            (ibig(1), false),
+        ], ibig(0), ibig(10));
+
+        match result {
+            Ok((lo, hi)) => assert_eq!((lo, hi), (ibig(0), ibig(0))),
+            Err(e) => panic!("expected (0, 0), but got error {:?}", e)
+        }
+    }
+
+    #[test]
+    fn test_find_bounds_from_bool_list_all_good_boundary() {
+        let result = find_bounds_from_bool_list(&[
+            (ibig(-1), true),
+            (ibig(0), true),
+        ], ibig(-10), ibig(0));
+
+        match result {
+            Err(InconsistencyError(s)) => assert_eq!("All values in [-10, 0] are good.", s),
+            Ok((lo, hi)) => assert_eq!((lo, hi), (ibig(0), ibig(0))),
+            Err(e) => panic!("expected InconsistencyError(\"All values in [-10, 0] are good.\"), but got error {:?}", e)
+        }
+    }
+
+    #[test]
+    fn test_find_bounds_from_bool_list_all_good() {
+        let result = find_bounds_from_bool_list(&[
+            (ibig(0), true),
+            (ibig(2), true),
+            (ibig(4), true),
+            (ibig(6), true),
+            (ibig(8), true)
+        ], ibig(-2), ibig(10));
+
+        match result {
+            Ok((lo, hi)) => assert_eq!((lo, hi), (ibig(9), ibig(10))),
+            Err(e) => panic!("expected (9, 10), but got error {:?}", e)
+        }
+    }
+
+    #[test]
+    fn test_find_bounds_from_bool_list_inconsistency() {
+        let result = find_bounds_from_bool_list(&[
+            (ibig(100), true),
+            (ibig(102), false),
+            (ibig(104), false),
+            (ibig(106), true),
+            (ibig(108), false)
+        ], ibig(98), ibig(110));
+
+        match result {
+            Ok(o) => panic!("expected InconsistencyError, but got positive result {:?}", o),
+            Err(InconsistencyError(s)) =>
+                assert_eq!(s, "Found a sequence of bad starting at 102, then another good at 106."),
+            Err(e) => panic!("expected InconsistencyError, but got {:?}", e)
+        }
+    }
+
+    #[test]
+    fn test_find_new_bounds() {
+        let result = find_new_bounds(HashMap::from([
+            (ibig(100), Ok(Good)),
+            (ibig(102), Ok(Good)),
+            (ibig(104), Ok(Bad)),
+            (ibig(106), Ok(Bad)),
+            (ibig(108), Ok(Bad))]), ibig(98), ibig(110));
+
+        match result {
+            Ok((lo, hi)) => assert_eq!((lo, hi), (ibig(103), ibig(104))),
+            Err(e) => panic!("expected (103, 104), but got error {:?}", e)
+        }
+    }
+
+    #[test]
+    fn test_find_new_bounds_err() {
+        let result = find_new_bounds(HashMap::from([
+            (ibig(100), Ok(Good)),
+            (ibig(102), Ok(Good)),
+            (ibig(104), Ok(Bad)),
+            (ibig(106), Err(PayloadError("it's broken".into()))),
+            (ibig(108), Ok(Bad))]), ibig(98), ibig(110));
+
+        match result {
+            Err(PayloadError(e)) => assert_eq!("it's broken", e),
+            x => panic!("expected PayloadError(\"it's broken\"), got {:?}", x)
+        }
+    }
+
+    #[test]
+    fn test_parasect() {
+        let result = parasect(
+            ParasectSettings::new(ibig(1), ibig(500), |x|
+                if x < ibig(320) { Continue(Good) } else { Continue(Bad) })
+        );
+
+        match result {
+            Ok(v) => assert_eq!(v, ibig(320)),
+            x => panic!("expected 320, got {:?}", x)
+        }
+    }
+
+    #[test]
+    fn test_parasect_stop() {
+        let result = parasect(
+            ParasectSettings::new(ibig(1), ibig(500), |x|
+                if x < ibig(15) { Stop("error".into()) } else { Continue(Bad) })
+        );
+
+        match result {
+            Err(PayloadError(s)) => assert_eq!(s, "error"),
+            x => panic!("expected PayloadError(\"error\"), got {:?}", x)
+        }
+    }
+
+    #[quickcheck]
+    fn qc_parasect(a: i16, b: i16, c: i16) -> TestResult {
+        let mut nums = [a, b, c];
+        nums.sort();
+        let [lo, lt, hi] = nums;
+
+        println!("testing {} {} {}", lo, lt, hi);
+
+        let result =
+            parasect(
+                ParasectSettings::new(lo, hi, |x|
+                    if x < IBig::from(lt) { Continue(Good) } else { Continue(Bad) }));
+
+        match result {
+            Ok(v) => {
+                println!("positive result {}", v);
+                TestResult::from_bool(v == IBig::from(lt))
+            },
+            e => {
+                println!("error result {:?}", e);
+                TestResult::failed()
+            }
         }
     }
 }
